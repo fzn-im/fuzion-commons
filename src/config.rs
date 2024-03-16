073ec89912acd280b384f40d77d5776f5ec47881
pika <enzi@braindead.io>
2024-03-16 18:03:57 -0500

    chore: pogress

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,6 @@
+use deadpool_postgres::{Manager, ManagerConfig, Pool as Deadpool, RecyclingMethod};
+
+use crate::db::PgPool;
 use crate::serde::{default_true, deserialize_log_level};
 
 #[derive(Clone, Debug, Deserialize)]
@@ -16,5 +19,47 @@ impl Default for LoggingConfig {
       log_level: slog::Level::Debug,
       log_file: None,
     }
+  }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct DatabaseConfig {
+  pub host: String,
+  pub port: u16,
+  pub user: String,
+  pub password: String,
+  pub name: String,
+}
+
+impl Default for DatabaseConfig {
+  fn default() -> Self {
+    Self {
+      host: "".to_owned(),
+      port: 5678u16,
+      name: "".to_owned(),
+      user: "".to_owned(),
+      password: "".to_owned(),
+    }
+  }
+}
+
+impl DatabaseConfig {
+  pub async fn get_db_pool(&self) -> Result<PgPool, ()> {
+    let mut pg_config = tokio_postgres::Config::new();
+    pg_config.user(&self.user);
+    pg_config.password(&self.password);
+    pg_config.dbname(&self.name);
+    pg_config.host(&self.host);
+    pg_config.port(self.port);
+
+    let manager = Manager::from_config(
+      pg_config,
+      tokio_postgres::NoTls,
+      ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+      },
+    );
+
+    Deadpool::builder(manager).build().map_err(|_| ())
   }
 }
