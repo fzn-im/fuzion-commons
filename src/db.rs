ff6d94664102290e59f7b6ddc60bbe2b764728cf
pika <enzi@braindead.io>
2024-03-20 15:17:30 -0500

    chore: cleanup unused

--- src/db.rs
+++ src/db.rs
@@ -1,17 +1,10 @@
-use std::collections::HashMap;
-use std::fmt::Display;
 use std::future::Future;
-use std::iter::FromIterator;
 use std::pin::Pin;
-use std::rc::Rc;
-use std::slice::Iter;
 
 use actix_web::*;
+use actix_web_thiserror::ResponseError;
 use futures::TryFutureExt;
 use thiserror::Error;
-use tokio_postgres::row::RowIndex;
-use tokio_postgres::types::FromSql;
-use tokio_postgres::Row;
 
 use crate::error::UnitError;
 
@@ -21,7 +14,6 @@ pub struct PgClient<'a> {
   inner: PgClientInner<'a>,
 }
 
-#[allow(dead_code)]
 impl<'a> PgClient<'a> {
   pub fn from_client(client: deadpool_postgres::Client) -> PgClient<'a> {
     client.into()
@@ -36,7 +28,6 @@ impl<'a> PgClient<'a> {
   }
 }
 
-#[allow(dead_code)]
 impl<'a> PgClient<'a> {
   pub async fn prepare(&self, query: &str) -> Result<tokio_postgres::Statement, PgClientError> {
     let stmt = match &self.inner {
@@ -141,17 +132,13 @@ impl<'a> PgClient<'a> {
 
 impl<'a> From<deadpool_postgres::Client> for PgClient<'a> {
   fn from(from: deadpool_postgres::Client) -> Self {
-    PgClient {
-      inner: PgClientInner::Client(from),
-    }
+    PgClient::from_client(from)
   }
 }
 
 impl<'a> From<deadpool_postgres::Transaction<'a>> for PgClient<'a> {
   fn from(from: deadpool_postgres::Transaction<'a>) -> Self {
-    PgClient {
-      inner: PgClientInner::Transaction(from),
-    }
+    PgClient::from_transaction(from)
   }
 }
 
@@ -187,179 +174,14 @@ impl<'a> FromRequest for PgClient<'a> {
   }
 }
 
-pub struct PgRow<'a> {
-  row: &'a Row,
-  columns: Rc<HashMap<&'a str, usize>>,
-}
-
-impl<'a> PgRow<'a> {
-  pub fn len(&self) -> usize {
-    self.row.len()
-  }
-
-  pub fn is_empty(&self) -> bool {
-    self.row.is_empty()
-  }
-
-  pub fn get<I, T>(&'a self, idx: I) -> T
-  where
-    I: RowIndex + Display,
-    T: FromSql<'a>,
-  {
-    self.row.get::<I, T>(idx)
-  }
-
-  pub fn get_named<T>(&'a self, name: &str) -> T
-  where
-    T: FromSql<'a>,
-  {
-    self.row.get::<_, T>(self.columns.get(name).unwrap())
-  }
-
-  pub fn get_named_json<T>(&'a self, name: &str) -> T
-  where
-    T: for<'b> serde::de::Deserialize<'b>,
-  {
-    let value = self
-      .row
-      .get::<_, serde_json::Value>(self.columns.get(name).unwrap());
-    serde_json::from_value(value).unwrap()
-  }
-
-  pub fn get_opt_named_json<T>(&'a self, name: &str) -> Option<T>
-  where
-    T: for<'b> serde::de::Deserialize<'b>,
-  {
-    let value = self
-      .row
-      .get::<_, Option<serde_json::Value>>(self.columns.get(name).unwrap());
-    value.map(|value| serde_json::from_value(value).unwrap())
-  }
-
-  pub fn from_vec(mut rows: Vec<&'a Row>) -> Vec<PgRow<'a>> {
-    let columns = Rc::new(
-      rows
-        .get(0)
-        .map(|row| {
-          HashMap::from_iter(
-            row
-              .columns()
-              .iter()
-              .enumerate()
-              .map(|(i, column)| (column.name(), i)),
-          )
-        })
-        .unwrap_or_else(|| HashMap::new()),
-    );
-
-    rows
-      .drain(..)
-      .map(|row| PgRow {
-        row,
-        columns: columns.clone(),
-      })
-      .collect::<Vec<PgRow<'a>>>()
-  }
-
-  pub fn from_iter(iter: &mut Iter<'a, Row>) -> Vec<PgRow<'a>> {
-    let mut rows = vec![];
-    let mut columns = None;
-
-    while let Some(row) = iter.next() {
-      if let None = columns {
-        columns = Some(Rc::new(HashMap::from_iter(
-          row
-            .columns()
-            .iter()
-            .enumerate()
-            .map(|(i, column)| (column.name(), i)),
-        )));
-      }
-
-      if let Some(ref columns) = columns {
-        rows.push(PgRow {
-          row,
-          columns: columns.clone(),
-        });
-      }
-    }
-
-    rows
-  }
-}
-
-impl<'a> From<&'a Row> for PgRow<'a> {
-  fn from(row: &'a Row) -> Self {
-    let columns = Rc::new(HashMap::from_iter(
-      row
-        .columns()
-        .iter()
-        .enumerate()
-        .map(|(i, column)| (column.name(), i)),
-    ));
-
-    Self { row, columns }
-  }
-}
-
-#[derive(Debug, Error)]
+#[derive(Debug, Error, ResponseError)]
 pub enum PgClientError {
-  #[error("internal error")]
+  #[error("internal_error")]
   InternalError,
   #[error("tokio_postgres error: {0}")]
-  PostgresError(tokio_postgres::Error),
-  #[error("deadpool pool error: {0}")]
-  DeadpoolPoolError(DeadpoolPoolError),
+  PostgresError(#[from] tokio_postgres::Error),
+  #[error("deadpool_pool error: {0}")]
+  DeadpoolPoolError(#[from] DeadpoolPoolError),
 }
 
 pub type DeadpoolPoolError = deadpool::managed::PoolError<tokio_postgres::Error>;
-
-impl From<tokio_postgres::Error> for PgClientError {
-  fn from(from: tokio_postgres::Error) -> Self {
-    PgClientError::PostgresError(from)
-  }
-}
-
-impl From<DeadpoolPoolError> for PgClientError {
-  fn from(from: DeadpoolPoolError) -> Self {
-    PgClientError::DeadpoolPoolError(from)
-  }
-}
-
-impl From<PgClientError> for Error {
-  fn from(from: PgClientError) -> Error {
-    HttpPostgresError(from)
-  }
-}
-
-#[allow(non_snake_case)]
-pub fn HttpPostgresError<T: Into<PgClientError> + Display>(err: T) -> actix_web::error::Error {
-  error!("Postgres error: {}.", &err);
-  actix_web::error::ErrorInternalServerError("")
-}
-
-pub struct ToSqlIterable {
-  pub values: Vec<Box<dyn tokio_postgres::types::ToSql + Sync + 'static>>,
-}
-
-impl ToSqlIterable {
-  pub fn new() -> ToSqlIterable {
-    ToSqlIterable { values: Vec::new() }
-  }
-
-  pub fn push<T: tokio_postgres::types::ToSql + Sync + 'static>(&mut self, value: T) {
-    self.values.push(Box::new(value));
-  }
-
-  pub fn len(&self) -> usize {
-    self.values.len()
-  }
-
-  pub fn to_vec(&self) -> Vec<&(dyn tokio_postgres::types::ToSql + Sync)> {
-    self
-      .values
-      .iter()
-      .map(|value| &**value as &(dyn tokio_postgres::types::ToSql + Sync))
-      .collect()
-  }
-}
