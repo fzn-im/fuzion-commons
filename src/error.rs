225956b72e7624e5358bf6d97af33abe5374731f
pika <enzi@braindead.io>
2024-06-10 23:39:58 -0500

    feat: error maps

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+
 use actix_web::http::StatusCode;
 use actix_web::{HttpResponse, ResponseError};
+use serde::ser::Serialize;
 
 #[derive(Debug)]
 pub struct UnitError;
@@ -17,5 +20,32 @@ impl ResponseError for UnitError {
 
   fn error_response(&self) -> HttpResponse {
     HttpResponse::InternalServerError().finish()
+  }
+}
+
+#[derive(Debug, Serialize)]
+pub struct ErrorMap {
+  error: String,
+  errors: HashMap<String, serde_json::Value>,
+  _type: String,
+}
+
+impl ErrorMap {
+  pub fn new<T: Into<String>>(error: T) -> Self {
+    Self {
+      error: error.into(),
+      errors: Default::default(),
+      _type: String::from("error_map"),
+    }
+  }
+
+  pub fn add_error<S: Serialize>(&mut self, field: &str, value: S) {
+    self
+      .errors
+      .insert(field.to_owned(), serde_json::to_value(&value).unwrap());
+  }
+
+  pub fn len(&self) -> usize {
+    self.errors.len()
   }
 }
